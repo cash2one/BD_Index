53428d396a3676c40888ca77c1f27a6171fdc007
doge <doge@doges-MacBook-Pro.local>
2017-07-11 13:01:28 +0800

    ok

---
+++ main.js
@@ -0,0 +1,34 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600, title: "Baidu Index - Injector" });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on("closed", function() {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function() {
+  app.quit();
+});
